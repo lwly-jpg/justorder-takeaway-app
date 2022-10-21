50a80f17a5ee548efb1fe8efb08addca9845295f
adonl <adnlwly@gmail.com>
2022-10-21 07:54:40 +0100

    add more functionality

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,17 +6,41 @@ class Takeaway
     @order = {}
   end
 
+  def interactive_menu
+  # Loads options on loop and takes user input from terminal
+    loop do
+      takeaway_options
+      process_input(STDIN.gets.chomp.downcase)
+    end
+  end
+
   def takeaway_options
-    # Prints out greeting and list of initial options for user
+  # Prints out greeting and list of initial options for user
     @terminal.puts "Hello, welcome to JustOrder. How can I help you today?"
     @terminal.puts "1. Show menu"
     @terminal.puts "2. New order"
+    @terminal.puts "3. Show current order"
+    @terminal.puts "5. Exit"
   end
 
   def show_menu
   # Prints out each item in the menu and it's price in a formatted string
     @menu.each do |item, price|
       @terminal.puts "#{item.capitalize} : £#{"%.2f" % price}"
+    end
+  end
+
+  def process_input(option_input)
+  # Processes user input
+    case option_input
+      when "1", "show menu"
+        show_menu
+      when "2", "new order"
+        add_item
+      when "3", "show order"
+        show_order
+      when "5", "exit"
+        exit
     end
   end
 
@@ -29,7 +53,13 @@ class Takeaway
   end
 
   def show_order
-  # Shows what's currently in the users order
-    @order
+  # Prints what's currently in the users order
+    @order.each do |item, price|
+      @terminal.puts "#{item.capitalize} : £#{"%.2f" % price}"
+    end
   end
 end
+
+## Code to run program ##
+my_order = Takeaway.new(Kernel)
+my_order.interactive_menu
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,9 +4,11 @@ describe Takeaway do
   context 'when initialized' do
     it 'greets user and shows list of options' do
       terminal = double :terminal
-      expect(terminal).to receive(:puts).with("Hello, welcome to JustOrder. How can I help you today?")
-      expect(terminal).to receive(:puts).with("1. Show menu")
-      expect(terminal).to receive(:puts).with("2. New order")
+      expect(terminal).to receive(:puts).with("Hello, welcome to JustOrder. How can I help you today?").ordered
+      expect(terminal).to receive(:puts).with("1. Show menu").ordered
+      expect(terminal).to receive(:puts).with("2. New order").ordered
+      expect(terminal).to receive(:puts).with("3. Show current order").ordered
+      expect(terminal).to receive(:puts).with("5. Exit").ordered
       new_customer = Takeaway.new(terminal)
       new_customer.takeaway_options
     end
@@ -18,6 +20,21 @@ describe Takeaway do
       expect(terminal).to receive(:puts).with("Salad : £2.00").ordered
       new_customer = Takeaway.new(terminal)
       new_customer.show_menu
+    end
+
+    it 'loads menu when requested by user' do
+      terminal = double :terminal
+      expect(terminal).to receive(:puts).with("Hello, welcome to JustOrder. How can I help you today?").ordered
+      expect(terminal).to receive(:puts).with("1. Show menu").ordered
+      expect(terminal).to receive(:puts).with("2. New order").ordered
+      expect(terminal).to receive(:puts).with("3. Show current order").ordered
+      expect(terminal).to receive(:puts).with("5. Exit").ordered
+      new_customer = Takeaway.new(terminal)
+      new_customer.takeaway_options
+      expect(terminal).to receive(:puts).with("Pizza : £3.00").ordered
+      expect(terminal).to receive(:puts).with("Burger : £2.00").ordered
+      expect(terminal).to receive(:puts).with("Salad : £2.00").ordered
+      new_customer.process_input("1")
     end
 
     it 'returns user order input' do
@@ -36,7 +53,8 @@ describe Takeaway do
       expect(terminal).to receive(:puts).with("1 x Burger added to your order")
       new_customer = Takeaway.new(terminal)
       new_customer.add_item
-      expect(new_customer.show_order).to eq("burger" => 2.0)
+      expect(terminal).to receive(:puts).with("Burger : £2.00").ordered
+      new_customer.show_order
     end
 
     it 'adds multiple items to order list' do
@@ -54,9 +72,46 @@ describe Takeaway do
       expect(terminal).to receive(:gets).and_return("salad")
       expect(terminal).to receive(:puts).with("1 x Salad added to your order")
       new_customer.add_item
-      expect(new_customer.show_order).to eq("burger" => 2.0, "pizza" => 3.0, "salad" => 2.0)
+      expect(terminal).to receive(:puts).with("Burger : £2.00").ordered
+      expect(terminal).to receive(:puts).with("Pizza : £3.00").ordered
+      expect(terminal).to receive(:puts).with("Salad : £2.00").ordered
+      new_customer.show_order
     end
 
+    it 'loads current order when requested by user' do
+      terminal = double :terminal
+      expect(terminal).to receive(:puts).with("Hello, welcome to JustOrder. How can I help you today?").ordered
+      expect(terminal).to receive(:puts).with("1. Show menu").ordered
+      expect(terminal).to receive(:puts).with("2. New order").ordered
+      expect(terminal).to receive(:puts).with("3. Show current order").ordered
+      expect(terminal).to receive(:puts).with("5. Exit").ordered
+      new_customer = Takeaway.new(terminal)
+      new_customer.takeaway_options
+      expect(terminal).to receive(:puts).with("Input what item you'd like to order:").ordered
+      expect(terminal).to receive(:gets).and_return("burger").ordered
+      expect(terminal).to receive(:puts).with("1 x Burger added to your order").ordered
+      new_customer.process_input("2")
+      expect(terminal).to receive(:puts).with("Input what item you'd like to order:").ordered
+      expect(terminal).to receive(:gets).and_return("pizza").ordered
+      expect(terminal).to receive(:puts).with("1 x Pizza added to your order").ordered
+      new_customer.add_item
+      expect(terminal).to receive(:puts).with("Burger : £2.00").ordered
+      expect(terminal).to receive(:puts).with("Pizza : £3.00").ordered
+      new_customer.process_input("3")
+    end
+
+    it 'exits and closes program when selected' do
+      terminal = double :terminal
+      expect(terminal).to receive(:puts).with("Hello, welcome to JustOrder. How can I help you today?").ordered
+      expect(terminal).to receive(:puts).with("1. Show menu").ordered
+      expect(terminal).to receive(:puts).with("2. New order").ordered
+      expect(terminal).to receive(:puts).with("3. Show current order").ordered
+      expect(terminal).to receive(:puts).with("5. Exit").ordered
+      new_customer = Takeaway.new(terminal)
+      new_customer.takeaway_options
+      result = new_customer.process_input("5")
+      expect{result}.to raise_exception(SystemExit)
+    end
 
   end
 
